6206838f9ded9409c4df66553462412dec99d2aa
FabioArnez <4230876+FabioArnez@users.noreply.github.com>
2025-07-21 11:46:07 +0000

    Deploying to gh-pages from @ FabioArnez/FabioArnez.github.io@dbe47e5f2b6ec8de7adb182fe98be57c7ce1164c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-repositories",
           title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "My github account and selected repositories.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
